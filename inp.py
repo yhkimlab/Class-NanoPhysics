cc199df44d86cbb7e2245d3ae0646d0b6151fbe7
Yong-Hoon Kim <yong.hoon.kim@kaist.ac.kr>
2020-03-25 11:53:04 +0900

    Delete inp.py

--- inp.py
+++
@@ -1,7 +0,0 @@
-# Input parameter                    
-L  = 100     # Box Length (Angstrom = 10^-10 m)
-n  = 201     # Number of grid points.  (spacing = L/(n-1))      
-num = 10     # Number of states to search  !!!! Must be integer
-Potential_Height = 4           # eV
-
-
